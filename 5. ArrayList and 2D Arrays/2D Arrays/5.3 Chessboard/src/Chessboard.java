ba6574b21d77e3991c53d540c109cd7d01cfd9b2
Yulieth Ojeda <yulieth9109@gmail.com>
2023-10-01 22:40:42 +0100

    Curso Finalizado

--- 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
+++ 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
@@ -1,7 +1,29 @@
+
 public class Chessboard {
     public static void main(String[] args) {
         char[][] chessboard = new char[8][8];
-
-        /* Create and print a chessboard. */
+        char color1 = '□';
+        char color2 = '■';
+        char finalColor = color1;
+        for (int ind1 = 0; ind1 < chessboard.length; ind1++) {
+            if (finalColor == color1) {
+                finalColor = color2;
+            } else {
+                finalColor = color1;
+            }
+            for (int ind2 = 0; ind2 < chessboard[ind1].length; ind2++) {
+                if (finalColor == color1) {
+                    finalColor = color2;
+                } else {
+                    finalColor = color1;
+                }
+                chessboard[ind1][ind2] = finalColor;
+                if (ind2 == 0 && ind1 > 0) {
+                    System.out.println("\r");
+                }
+                System.out.print(finalColor);
+            }
+        }
+        System.out.println("\r");
     }
 }
--- 5. ArrayList and 2D Arrays/ArrayList/5.2 Average Calculator/src/AverageCalculator.java
+++ 5. ArrayList and 2D Arrays/ArrayList/5.2 Average Calculator/src/AverageCalculator.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 
 public class AverageCalculator {
     public static double getAverageOf(ArrayList<Integer> numbers) {
-        /* TODO */
+		double average = 0;
+		for (int index = 0; index < numbers.size(); index++) {
+			average = average + numbers.get(index);
+		}
+		return (average / numbers.size());
     }
 }
